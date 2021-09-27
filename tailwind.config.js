4e9043f21b470655e7be8024cca138b83a92d8cb
mi-skam <maksim.codes@mailbox.org>
2021-09-27 10:19:28 +0000

    tailwindcss purge needs to be done from dist/

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  purge: ["./dist/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  // purge: false,
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
